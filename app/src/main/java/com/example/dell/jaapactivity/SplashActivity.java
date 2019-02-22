c0b1962ff378c9152f6605d8186951401c81b262
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2019-02-22 14:48:36 +0530

    some code changes enhancements

--- app/src/main/java/com/example/dell/jaapactivity/SplashActivity.java
+++ app/src/main/java/com/example/dell/jaapactivity/SplashActivity.java
@@ -4,16 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 public class SplashActivity extends AppCompatActivity {
     final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
     private final int SPLASH_DISPLAY_LENGTH = 2000;
+    ImageView appname;
+    ImageView logoapp;
+
     private int RECORD_AUDIO_PERMISSION = 200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        appname = findViewById(R.id.appnamesp);
+        logoapp = findViewById(R.id.logoapp);
+        YoYo.with(Techniques.Bounce).duration(1000).playOn(appname);
+        YoYo.with(Techniques.FlipInX).duration(1300).playOn(logoapp);
+
 
         new Handler().postDelayed(new Runnable(){
             @Override
